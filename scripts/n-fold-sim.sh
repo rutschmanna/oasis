b29fd49430146605c624c407bee762bfffc7efa3
A.R. <andri.rutschmann@uni-konstanz.de>
2025-08-05 21:25:57 +0000

    updated sim scripts

--- scripts/n-fold-sim.sh
+++ scripts/n-fold-sim.sh
@@ -1,28 +1,37 @@
 #!/bin/bash
 
-# for i in 1 6;
-# do
-#     for j in $(seq 1 20);
-#     do
-#     	python scripts/14_sim_control.py --model-name qwen --time-steps $1 --subreddit "$i" --topic $j --clock-factor 60
-    
-#     done
-# done
-
-# for i in 2 5;
-# do
-#     for j in $(seq 1 20);
-#     do
-#     	python scripts/14_sim_moderation.py --model-name qwen --time-steps $1 --subreddit "$i" --topic $j --clock-factor 60
-    
-#     done
-# done
-
-for i in 3 4;
+for r in $(seq 4 9);
 do
-    for j in $(seq 1 20);
+    for i in 1 6;
     do
-    	python scripts/14_sim_incentives.py --model-name qwen --time-steps $1 --subreddit "$i" --topic $j --clock-factor 60
-    
+        for j in $(seq 1 20);
+        do
+        	python scripts/14_sim_control.py --model-name qwen --time-steps $1 --subreddit "$i" --topic $j --clock-factor 60 --n-run $r
+        
+        done
     done
 done
+
+# for r in 1 10;
+# do
+#     for i in 2 5;
+#     do
+#         for j in $(seq 1 20);
+#         do
+#         	python scripts/14_sim_moderation.py --model-name qwen --time-steps $1 --subreddit "$i" --topic $j --clock-factor 60 --n-run $r
+        
+#         done
+#     done
+# done
+
+# for r in 1 10;
+# do
+#     for i in 3 4;
+#     do
+#         for j in $(seq 1 20);
+#         do
+#         	python scripts/14_sim_incentives.py --model-name qwen --time-steps $1 --subreddit "$i" --topic $j --clock-factor 60 -n-run $r
+        
+#         done
+#     done
+# done
